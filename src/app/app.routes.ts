205924005b05eef4c29f3effe5688146f70b6027
tiagoafonsonatixis <tiago.afonso.natixis@msft.cesae.pt>
2024-11-11 14:52:53 +0000

    Game details component and HTML

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { GameDetailsComponent } from './game-details/game-details.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'game/:id', component: GameDetailsComponent }
+];
--- src/app/game-details/game-details.component.ts
+++ src/app/game-details/game-details.component.ts
@@ -1,12 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { GameDetails } from '../models/game-details.model';
+import { LojaService } from '../services/loja.service';
+import { CommonModule } from '@angular/common';
+import { of } from 'rxjs';  // Import 'of' to handle errors
 
 @Component({
   selector: 'app-game-details',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './game-details.component.html',
-  styleUrl: './game-details.component.scss'
+  styleUrls: ['./game-details.component.scss']
 })
-export class GameDetailsComponent {
+export class GameDetailsComponent implements OnInit {
+  gameDetails: GameDetails | null = null;
+  errorMessage: string | null = null;
 
+  constructor(
+    private gameService: LojaService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    const gameId = this.route.snapshot.paramMap.get('id');
+    if (gameId) {
+      this.fetchGameDetails(gameId);
+    }
+  }
+
+  fetchGameDetails(id: string): void {
+    this.gameService.getGameDetails(id).subscribe({
+      next: (data) => {
+        this.gameDetails = data;
+        this.errorMessage = null;
+      },
+      error: () => {
+        this.gameDetails = null;
+        this.errorMessage = 'Game not found. Please check the game ID and try again.';
+      }
+    });
+  }
 }
