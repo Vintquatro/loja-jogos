61c48a670037ef1ae2fe83ccc18159da9e93e32f
rodrigosousa <rodrigosousa1105@gmail.com>
2024-11-12 09:55:40 +0000

    Search bar added

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -3,19 +3,22 @@ import { LojaService } from '../services/loja.service';
 import { CommonModule } from '@angular/common';
 import { Game } from '../models/game' 
 import { RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, FormsModule],
   templateUrl: './main-page.component.html',
   styleUrls: ['./main-page.component.scss']
 })
 export class MainPageComponent implements OnInit {
 
   gamesList: Game[] = [];
+  filteredGames: Game[] = [];
   loading: boolean = true;
   error: string = '';
+  searchQuery: string = '';
 
   constructor(private lojaService: LojaService) { }
 
@@ -27,12 +30,25 @@ export class MainPageComponent implements OnInit {
     this.lojaService.getGamesList().subscribe({
       next: (data) => {
         this.gamesList = data;
+        this.filteredGames = data;
         this.loading = false;
       },
       error: (err) => {
-        this.error = 'Erro ao carregar a lista de jogos';
+        this.error = 'Error loading the list of games.';
         this.loading = false;
       }
     });
+  }
+
+  filterGames(): void {
+    if (this.searchQuery.trim() === '') {
+      this.filteredGames = this.gamesList; 
+    } else {
+      this.filteredGames = this.gamesList.filter(game =>
+        game.title.toLowerCase().includes(this.searchQuery.toLowerCase()) || 
+        game.genre.toLowerCase().includes(this.searchQuery.toLowerCase()) ||
+        game.platform.toLowerCase().includes(this.searchQuery.toLowerCase())
+      );
+    }
   }
 }
