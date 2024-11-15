c57f3e74e1aba96a84d8b7200438ea451ebb4401
tiagoafonsonatixis <tiago.afonso.natixis@msft.cesae.pt>
2024-11-15 16:18:32 +0000

    Align to bottom screenshots and button

--- src/app/game-details/game-details.component.ts
+++ src/app/game-details/game-details.component.ts
@@ -3,35 +3,46 @@ import { ActivatedRoute } from '@angular/router';
 import { GameDetails } from '../models/game-details.model';
 import { LojaService } from '../services/loja.service';
 import { CommonModule } from '@angular/common';
-import { of } from 'rxjs';  // Import 'of' to handle errors
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-game-details',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './game-details.component.html',
   styleUrls: ['./game-details.component.scss']
 })
 export class GameDetailsComponent implements OnInit {
   gameDetails: GameDetails | null = null;
+  userProfile: any = {};
+  currentListId: string | null = null;
   errorMessage: string | null = null;
   zoomedImage: string | null = null;
   isZoomed = false;
-
   constructor(
-    private gameService: LojaService,
+    private lojaService: LojaService,
     private route: ActivatedRoute
-  ) {}
+  ) { }
 
   ngOnInit(): void {
     const gameId = this.route.snapshot.paramMap.get('id');
     if (gameId) {
       this.fetchGameDetails(gameId);
     }
+
+    this.lojaService.getProfile().subscribe(
+      (data) => {
+        this.userProfile = data;
+        this.currentListId = this.getGameCurrentList(gameId);
+      },
+      (error) => {
+        console.error('Error fetching user profile:', error);
+      }
+    );
   }
 
   fetchGameDetails(id: string): void {
-    this.gameService.getGameDetails(id).subscribe({
+    this.lojaService.getGameDetails(id).subscribe({
       next: (data) => {
         this.gameDetails = data;
         this.errorMessage = null;
@@ -50,5 +61,47 @@ export class GameDetailsComponent implements OnInit {
 
   closeZoom(): void {
     this.isZoomed = false;
+  }
+
+  onListChange(event: Event): void {
+    const selectElement = event.target as HTMLSelectElement;
+    const targetListId = selectElement.value;
+
+    if (targetListId === this.currentListId)
+      return;
+
+    this.moveGameToList(targetListId);
+  }
+
+  moveGameToList(targetListId: string): void {
+    if (this.gameDetails) {
+      const targetList = this.userProfile.lists.find((list: any) => list.id === targetListId);
+      const currentList = this.userProfile.lists.find((list: any) => list.id === this.currentListId);
+
+      if (targetList && currentList) {
+        currentList.gamesIds = currentList.gamesIds.filter((id: string) => id !== this.gameDetails?.id);
+        targetList.gamesIds.push(this.gameDetails.id);
+
+        this.updateProfile();
+        this.currentListId = targetListId;
+      }
+    }
+  }
+
+  updateProfile(): void {
+    this.lojaService.updateProfile(this.userProfile).subscribe(
+      (updatedData) => {
+        console.log('Profile updated successfully:', updatedData);
+      },
+      (error) => {
+        console.error('Error updating profile:', error);
+      }
+    );
+  }
+
+  getGameCurrentList(gameId: string | null): string | null {
+    if (!gameId || !this.userProfile?.lists) return null;
+    const list = this.userProfile.lists.find((list: any) => list.gamesIds.includes(gameId));
+    return list ? list.id : null;
   }
 }
